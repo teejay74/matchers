cc723e7dd7e07a0cbd4b9cc9dac3fbc8ee2f39de
teejay74 <teejay744@gmail.com>
2020-09-23 15:29:43 +0500

    task fix for appveyor

--- src/js/__tests__/health.test.js
+++ src/js/__tests__/health.test.js
@@ -1,13 +1,13 @@
 import getHealthSort from "../health";
 test('testing healthSort', () => {
   expect(getHealthSort([
-    {name: 'мечник', health: 10},
-    {name: 'маг', health: 100},
-    {name: 'лучник', health: 80},
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
   ])).toEqual([
-    {name: 'маг', health: 100},
-    {name: 'лучник', health: 80},
-    {name: 'мечник', health: 10},
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
   ])
 
 })
